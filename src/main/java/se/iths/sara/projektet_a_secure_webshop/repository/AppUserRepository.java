f308010d8e261b25298b15da2f48d0e928cf9c1a
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-04-08 12:02:50 +0200

    Add user registration+ändringar

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/repository/AppUserRepository.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/repository/AppUserRepository.java
@@ -6,5 +6,5 @@ import se.iths.sara.projektet_a_secure_webshop.model.AppUser;
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByEmail(String email);
 }
