db68235d19aa81667a6f73d0bd24349f62d40202
MartinGruter <Martin.Grter@iths.se>
2026-04-09 11:30:04 +0200

    Created attributes, getters/setters and OneToMany/ManyToOne for Cart and CartItem.

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/model/Cart.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/model/Cart.java
@@ -1,7 +1,17 @@
 package se.iths.sara.projektet_a_secure_webshop.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
 import java.util.List;
 
+@Entity
 public class Cart {
-    List<CartItem> items;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems;
 }
