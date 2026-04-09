0917c6fa3d12f6dd53786e4776fd9fbddca8cfde
ynsuldg <yns.uldg@hotmail.com>
2026-04-09 12:44:21 +0200

    Product repository added...

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/repository/ProductRepository.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package se.iths.sara.projektet_a_secure_webshop.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import se.iths.sara.projektet_a_secure_webshop.model.Product;
 
-import java.util.List;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findByUser_id(Long id);
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
