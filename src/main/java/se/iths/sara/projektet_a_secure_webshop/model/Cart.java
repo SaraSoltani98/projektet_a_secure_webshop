00d97cd9e06f7869f1b1fe1c3eff3ca36e88e261
MartinGruter <Martin.Grter@iths.se>
2026-04-11 15:13:34 +0200

    Added CartItemService and changed double in Product to BigDecimal.

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/model/Cart.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/model/Cart.java
@@ -3,17 +3,42 @@ package se.iths.sara.projektet_a_secure_webshop.model;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private BigDecimal totalAmount;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CartItem> cartItems;
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    public void addCartItem(CartItem cartItem) {
+        this.cartItems.add(cartItem);
+        cartItem.setCart(this);
+        updateTotalAmount();
+    }
+
+    public void removeCartItem(CartItem cartItem) {
+        this.cartItems.remove(cartItem);
+        cartItem.setCart(null);
+        updateTotalAmount();
+    }
+
+    public void updateTotalAmount() {
+        this.totalAmount = cartItems.stream().map(item -> {
+            BigDecimal unitPrice = item.getUnitPrice();
+            if (unitPrice == null) {
+                return BigDecimal.ZERO;
+            }
+            return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
+        }).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
 
     public Long getId() {
         return id;
