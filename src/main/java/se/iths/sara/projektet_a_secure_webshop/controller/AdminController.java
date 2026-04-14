cd1622184a762862eeff77b8ef344dc340746eae
ynsuldg <yns.uldg@hotmail.com>
2026-04-14 11:34:47 +0200

    Admin controller class updated... (Few lines removed)

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/controller/AdminController.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/controller/AdminController.java
@@ -1,9 +1,7 @@
 package se.iths.sara.projektet_a_secure_webshop.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,20 +21,12 @@ public class AdminController {
 
     @GetMapping
     public String showAdminPage(Model model) {
-        model.addAttribute("product", new Product());
         model.addAttribute("products", productService.getAllProducts());
         return "admin";
     }
 
     @PostMapping("/products")
-    public String createProduct(@Valid @ModelAttribute("product") Product product,
-                                BindingResult bindingResult,
-                                Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("products", productService.getAllProducts());
-            return "admin";
-        }
-
+    public String createProduct(@ModelAttribute("product") Product product) {
         productService.createProduct(product);
         return "redirect:/admin";
     }
