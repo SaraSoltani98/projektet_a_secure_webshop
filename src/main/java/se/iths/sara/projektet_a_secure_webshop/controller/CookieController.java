9e9a69404ac2cd4579e9913a73fb36e9e3763520
AnwarAsry <anwareducation03@gmail.com>
2026-04-15 16:37:19 +0200

    Cookie page

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/controller/CookieController.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/controller/CookieController.java
@@ -2,20 +2,28 @@ package se.iths.sara.projektet_a_secure_webshop.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import static se.iths.sara.projektet_a_secure_webshop.util.CookieUtil.setConsentCookie;
 
 @Controller
+@RequestMapping("/cookies")
 public class CookieController {
 
-    @PostMapping("/cookies/consent")
+    @GetMapping
+    public String cookiePage() {
+        return "cookie";
+    }
+
+    @PostMapping("/consent")
     public String consent(HttpServletResponse response) {
         setConsentCookie(response, "true");
         return "redirect:/";
     }
 
-    @PostMapping("/cookies/dissent")
+    @PostMapping("/dissent")
     public String dissent(HttpServletResponse response) {
         setConsentCookie(response, "false");
         return "redirect:/";
