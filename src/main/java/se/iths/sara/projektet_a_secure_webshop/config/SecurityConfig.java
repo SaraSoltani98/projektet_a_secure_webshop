6cec5313e40ecd506a46596cc214a4ae423763bb
MartinGruter <Martin.Grter@iths.se>
2026-04-21 10:05:40 +0200

    Added a messenger dependency in pom.xml. Added OrderService and OrderController. Few changes in cart and maven.yml.

--- src/main/java/se/iths/sara/projektet_a_secure_webshop/config/SecurityConfig.java
+++ src/main/java/se/iths/sara/projektet_a_secure_webshop/config/SecurityConfig.java
@@ -39,14 +39,7 @@ public class SecurityConfig {
                         })
                         .permitAll()
                 )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .clearAuthentication(true)
-                        .permitAll()
-                )
+
                 .exceptionHandling(exception -> exception
                         .accessDeniedHandler((request, response, accessDeniedException) ->
                                 response.sendRedirect("/"))
